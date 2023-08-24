7fe61c6c558e4019c2e04cfbce236ff00c05ed7e
Mavluda <rahimqulimavluda@gmail.com>
2023-08-24 18:26:33 +0500

    Fix for README file

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,13 @@
-alert(1);
-[0,3].pus
+var name = "Malis";
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+pi = 5;
+
+let leftBorderWidth = 200;
+
+// number
+// string - "", '', ``
+// true/false
+// null
